52f01a3c4489d0ef34b447982ba26cfaa8609c4d
huurray <huurray@gmail.com>
2018-10-12 16:19:41 +0900

    first finish

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import styled from 'styled-components';
 import withStyles from './styles';
 
 // //sections
-// import Section1 from './components/Section1';
-// import Section2 from './components/Section2';
-// import Section3 from './components/Section3';
+import Section1 from './components/Section1';
+import Section2 from './components/Section2';
+import Section3 from './components/Section3';
 
-// import Footer from './components/Footer';
-// import Header from './components/Heading';
-// import DownloadApp from './components/DownloadApp';
+import Footer from './components/Footer';
+import Header from './components/Heading';
+import DownloadApp from './components/DownloadApp';
 
 const Container = styled.div``;
 
@@ -29,13 +29,12 @@ class App extends React.Component {
   render() {
     return (
       <Container>
-        sdfsdf
-        {/* <Header scrollToDomRef={this.scrollToDomRef} />
+        <Header scrollToDomRef={this.scrollToDomRef} />
         <Section1 />
         <Section2 />
         <Section3 />
         <DownloadApp ref={this.downloadApp} />
-        <Footer /> */}
+        <Footer />
       </Container>
     );
   }
